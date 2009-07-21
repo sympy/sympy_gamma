59b7cf0978ab2be9a1addbeeaa067fecdef12fe9
Ondrej Certik <ondrej@certik.cz>
2009-07-21 12:11:04 -0600

    TextArea added

--- media/Hello.py
+++ media/Hello.py
@@ -4,6 +4,7 @@ from pyjamas.ui.Button import Button
 from pyjamas.ui.HTML import HTML
 from pyjamas.ui.Label import Label
 from pyjamas import Window
+from pyjamas.ui.TextArea import TextArea
 
 def greet(fred):
     print "greet button"
@@ -14,7 +15,9 @@ if __name__ == '__main__':
     b = Button("Click me", greet, StyleName='teststyle')
     h = HTML("<b>Hello World</b> (html)", StyleName='teststyle')
     l = Label("Hello World (label)", StyleName='teststyle')
+    t = TextArea()
     RootPanel().add(b)
     RootPanel().add(h)
     RootPanel().add(l)
+    RootPanel().add(t)
     pyjd.run()
