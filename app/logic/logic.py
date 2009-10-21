68f6733c464bbdb31118ed80dccba15c191d05ba
Ondrej Certik <ondrej@certik.cz>
2009-10-21 15:06:31 -0700

    Format cells

--- app/logic/logic.py
+++ app/logic/logic.py
@@ -6,12 +6,18 @@ class SymPyGamma(object):
         r = self.try_sympy(s)
         if r:
             return r
-        return [{"input": s}, {"result": "Can't handle the input."}]
+        return [
+                {"title": "Input", "input": s,
+                    "output": "Can't handle the input."},
+                ]
 
     def try_sympy(self, s):
         a = Eval()
         r = a.eval(s, use_none_for_exceptions=True)
         if r:
-            return [{"input": s}, {"result": r}]
+            return [
+                    {"title": "Input", "input": s},
+                    {"title": "Eval", "input": s, "output": r},
+                    ]
         else:
             return None
