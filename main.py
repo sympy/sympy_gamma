3d61f3e5fe6e088bde9185b298f31d81a3ed133c
Ondrej Certik <ondrej@certik.cz>
2009-10-21 16:43:30 -0700

    Update Django to work on the app engine

--- main.py
+++ main.py
@@ -1,32 +1,35 @@
-import logging, os
+import logging, os, sys
 
 # Google App Engine imports.
 from google.appengine.ext.webapp import util
 
-# Force Django to reload its settings.
-from django.conf import settings
-settings._target = None
+# Remove the standard version of Django.
+for k in [k for k in sys.modules if k.startswith('django')]:
+  del sys.modules[k]
 
-# Must set this env var before importing any part of Django
+# Force sys.path to have our own directory first, in case we want to import
+# from it.
+sys.path.insert(0, os.path.abspath(os.path.dirname(__file__)))
+
+# Must set this env var *before* importing any part of Django
 os.environ['DJANGO_SETTINGS_MODULE'] = 'settings'
 
-import logging
 import django.core.handlers.wsgi
 import django.core.signals
 import django.db
 import django.dispatch.dispatcher
 
 def log_exception(*args, **kwds):
-  logging.exception('Exception in request:')
+ logging.exception('Exception in request:')
 
 # Log errors.
-django.dispatch.dispatcher.connect(
-   log_exception, django.core.signals.got_request_exception)
+#django.dispatch.dispatcher.connect(
+#   log_exception, django.core.signals.got_request_exception)
 
 # Unregister the rollback event handler.
-django.dispatch.dispatcher.disconnect(
-    django.db._rollback_on_exception,
-    django.core.signals.got_request_exception)
+#django.dispatch.dispatcher.disconnect(
+#    django.db._rollback_on_exception,
+#    django.core.signals.got_request_exception)
 
 def main():
   # Create a Django application for WSGI.
--- settings.py
+++ settings.py
@@ -74,8 +74,8 @@ TEMPLATE_DIRS = (
 )
 
 INSTALLED_APPS = (
-    'django.contrib.auth',
-    #    'django.contrib.contenttypes',
-    'django.contrib.sessions',
+        #'django.contrib.auth',
+    'django.contrib.contenttypes',
+    #'django.contrib.sessions',
     #'django.contrib.sites',
 )
