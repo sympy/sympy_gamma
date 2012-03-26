e87765b5af2f1fca9d8561e17d41883c6e1a9a12
André Luiz Guimarães de Souza Leite <andre.luiz.leite@ccc.ufcg.edu.br>
2012-03-26 20:58:53 -0300

    Changed the logic so that when there are more than one free variable nothing happens.

--- app/logic/logic.py
+++ app/logic/logic.py
@@ -28,7 +28,7 @@ class SymPyGamma(object):
             code = """\
 s = %s
 a = s.atoms(Symbol)
-if len(a):
+if len(a) == 1:
     x = a.pop()
     result = %s
 else:
@@ -36,38 +36,42 @@ else:
 pprint(result)
 """
             var = a.eval(code % (s, 'x'), use_none_for_exceptions=True)
-            var = var.replace("\nNone", "")
+            # Come up with a solution to use all variables if more than 1
+            # is entered.
             line = "simplify(%s)"
             simplified = a.eval(line % s, use_none_for_exceptions=True)
             r = a.eval(code % (s, line % s), use_none_for_exceptions=True)
-            if simplified and simplified != "None" and simplified != s:
+            if simplified and simplified != "None" and simplified.replace(" ", "") != s:
                 result.append(
                         {"title": "Simplification", "input": simplified,
                             "output": r})
-            line = "solve(%s, {_var})".format(_var=var)
-            r = a.eval(code % (s, (line % s)), use_none_for_exceptions=True)
-            if r and r != "None":
-                result.append(
-                        {"title": "Roots", "input": line % simplified,
-                            "output": r})
-            line = "diff(%s, {_var})".format(_var=var)
-            r = a.eval(code % (s, line % s), use_none_for_exceptions=True)
-            if r and r != "None":
-                result.append(
-                        {"title": "Derivative", "input": (line % simplified),
-                            "output": r})
-            line = "integrate(%s, {_var})".format(_var=var)
-            r = a.eval(code % (s, line % s), use_none_for_exceptions=True)
-            if r and r != "None":
-                result.append(
-                        {"title": "Indefinite integral", "input": line % simplified,
-                            "output": r})
-            line = "series(%s, {_var}, 0, 10)".format(_var=var)
-            r = a.eval(code % (s, line % s), use_none_for_exceptions=True)
-            if r and r != "None":
-                result.append(
-                        {"title": "Series expansion around 0", "input": line % simplified,
-                            "output": r})
+            if var != None: # See a better way to do this.
+                var = var.replace("None", "").replace("\n", "")
+                if len(var):
+                    line = "solve(%s, {_var})".format(_var=var)
+                    r = a.eval(code % (s, (line % s)), use_none_for_exceptions=True)
+                    if r and r != "None":
+                        result.append(
+                                {"title": "Roots", "input": line % simplified,
+                                    "output": r})
+                    line = "diff(%s, {_var})".format(_var=var)
+                    r = a.eval(code % (s, line % s), use_none_for_exceptions=True)
+                    if r and r != "None":
+                        result.append(
+                                {"title": "Derivative", "input": (line % simplified),
+                                    "output": r})
+                    line = "integrate(%s, {_var})".format(_var=var)
+                    r = a.eval(code % (s, line % s), use_none_for_exceptions=True)
+                    if r and r != "None":
+                        result.append(
+                                {"title": "Indefinite integral", "input": line % simplified,
+                                    "output": r})
+                    line = "series(%s, {_var}, 0, 10)".format(_var=var)
+                    r = a.eval(code % (s, line % s), use_none_for_exceptions=True)
+                    if r and r != "None":
+                        result.append(
+                                {"title": "Series expansion around 0", "input": line % simplified,
+                                    "output": r})
             for item in result:
                 for k in item:
                         item[k] = item[k].replace('None', '')
