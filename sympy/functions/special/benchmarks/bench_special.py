168495a35dc2a5260613d41acf8f95f685a95676
David Li <li.davidm96@gmail.com>
2012-09-08 03:13:42 +0000

    Remove old SymPy

--- sympy/functions/special/benchmarks/bench_special.py
+++
@@ -1,7 +0,0 @@
-from sympy import symbols
-from sympy.functions.special.spherical_harmonics import Ylm
-
-x,y = symbols('xy')
-
-def timeit_Ylm_xy():
-    Ylm(1,1, x,y)
--- sympy/series/__init__.py
+++
@@ -1,8 +0,0 @@
-"""A module that handles series: find a limit, order the series etc.
-"""
-from order import Order
-from limits import limit, Limit
-from gruntz import gruntz
-from series import series
-
-O = Order
