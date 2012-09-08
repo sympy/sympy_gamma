7875388305cd0ab194ccfcc387595aec50af6b80
David Li <li.davidm96@gmail.com>
2012-09-08 23:14:42 +0000

    Use MathJax only when specified

--- app/logic/logic.py
+++ app/logic/logic.py
@@ -32,8 +32,9 @@ class SymPyGamma(object):
         r = a.eval(s, use_none_for_exceptions=False)
         if r is not None:
             result = [
-                    {"title": "Input", "input": s},
-                    {"title": "SymPy", "input": s, "output": r},
+                {"title": "Input", "input": s},
+                {"title": "SymPy", "input": s, "output": r,
+                 "use_mathjax": False},
                     ]
             code = """\
 s = %s
@@ -54,7 +55,7 @@ result
             if simplified != "None" and simplified != s:
                 result.append(
                         {"title": "Simplification", "input": simplified,
-                            "output": latex(r)})
+                         "output": latex(r), "use_mathjax": True})
             if var != None: # See a better way to do this.
                 var = var.replace("None", "").replace("\n", "")
                 if len(var):
@@ -62,8 +63,9 @@ result
                     r = sympify(a.eval(code % (s, (line % s)), use_none_for_exceptions=True))
                     if r and r != "None":
                         result.append(
-                                {"title": "Roots", "input": line % simplified,
-                                 "pre_output": latex(var), "output": latex(r)})
+                            {"title": "Roots", "input": line % simplified,
+                             "pre_output": latex(var), "output": latex(r),
+                             "use_mathjax": True})
 
                     line = "diff(%s, {_var})".format(_var=var)
                     r = sympify(a.eval(code % (s, line % s), use_none_for_exceptions=True))
@@ -71,7 +73,7 @@ result
                         result.append(
                                 {"title": "Derivative", "input": (line % simplified),
                                  "pre_output": latex(Derivative(s, Symbol(var))),
-                                 "output": latex(r)})
+                                 "output": latex(r), "use_mathjax": True})
 
                     line = "integrate(%s, {_var})".format(_var=var)
                     r = sympify(a.eval(code % (s, line % s), use_none_for_exceptions=True))
@@ -79,14 +81,14 @@ result
                         result.append(
                                 {"title": "Indefinite integral", "input": line % simplified,
                                  "pre_output": latex(Integral(s,Symbol(var))),
-                                 "output": latex(r)})
+                                 "output": latex(r), "use_mathjax": True})
 
                     line = "series(%s, {_var}, 0, 10)".format(_var=var)
                     r = sympify(a.eval(code % (s, line % s), use_none_for_exceptions=True))
                     if r and r != "None":
                         result.append(
                                 {"title": "Series expansion around 0", "input": line % simplified,
-                                 "output": latex(r)})
+                                 "output": latex(r), "use_mathjax": True})
             return result
         else:
             return None
