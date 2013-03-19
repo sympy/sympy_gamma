48e470133c52194b0c7cc3b2650db034bf39c0d1
David Li <li.davidm96@gmail.com>
2013-03-19 12:23:22 -0700

    Cleanup and update files

--- main.py
+++ main.py
@@ -1,11 +1,4 @@
-import logging, os, sys
-
-# Google App Engine imports.
-from google.appengine.ext.webapp import util
-
-# Remove the standard version of Django.
-for k in [k for k in sys.modules if k.startswith('django')]:
-  del sys.modules[k]
+import os, sys
 
 # Force sys.path to have our own directory first, in case we want to import
 # from it.
@@ -15,20 +8,5 @@ sys.path.insert(0, os.path.abspath(os.path.dirname(__file__)))
 os.environ['DJANGO_SETTINGS_MODULE'] = 'settings'
 
 import django.core.handlers.wsgi
-import django.core.signals
-import django.db
-import django.dispatch.dispatcher
-
-def log_exception(*args, **kwds):
- logging.exception('Exception in request:')
-
-# Log errors.
-#django.dispatch.dispatcher.connect(
-#   log_exception, django.core.signals.got_request_exception)
-
-# Unregister the rollback event handler.
-#django.dispatch.dispatcher.disconnect(
-#    django.db._rollback_on_exception,
-#    django.core.signals.got_request_exception)
 
 application = django.core.handlers.wsgi.WSGIHandler()
