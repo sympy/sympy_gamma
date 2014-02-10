f420b2b05bb67cfe906bbd177024d79f5085d3a6
sahilshekhawat <sahilshekhawat01@gmail.com>
2014-02-10 17:22:00 +0530

    NO changes Done.They are still the original models.

--- app/models.py
+++ app/models.py
@@ -2,12 +2,7 @@ from google.appengine.ext import ndb
 
 class Query(ndb.Model):
     text = ndb.StringProperty()
-    date = ndb.DateTimeProperty(auto_now_add = True)
+    date = ndb.DateTimeProperty(auto_now_add=True)
     user_id = ndb.StringProperty()
 
-# this class can be used to store result in the databases.
-# but that costs money. so i am avoiding it.:)
 
-class Notebook(ndb.Model):
-    result_json = ndb.JsonProperty(indexed = False)
-    timestamp = ndb.DateTimeProperty(auto_now_add = True, indexed = True)
