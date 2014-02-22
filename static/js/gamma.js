d54576e6d9f577cd6dfaf578d9f80c7cbcd4f39a
sahilshekhawat <sahilshekhawat01@gmail.com>
2014-02-22 09:21:34 +0530

    Revert "Revert "made click function for the entire div.""
    This reverts commit 372bc0f044453d3ca9d09b8f11fc1a8ae00e15ef.

--- static/js/gamma.js
+++ static/js/gamma.js
@@ -24,7 +24,7 @@ function setupExamples() {
         }
     });
 
-    $('.example-group h3').click(function(e) {
+    $('.example-group').click(function(e) {
         var header = $(e.target);
         var contents = header.siblings('div.contents');
 
