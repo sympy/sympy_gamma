ca21183277643f7b9751b58696ce3b4514e4e96e
David Li <li.davidm96@gmail.com>
2014-05-09 16:45:15 -0700

    Run PhantomJS tests on Travis

--- travis.py
+++ travis.py
@@ -1,2 +1,13 @@
 import nose
-nose.main()
+import os
+import os.path
+import subprocess
+
+if __name__ == '__main__':
+    print("Running PhantomJS Tests")
+    gamma_directory = os.path.dirname(os.path.realpath(__file__))
+    subprocess.call(['casperjs', 'test', os.path.join(gamma_directory, 'app/test/')])
+
+    print
+    print("Running Python Unittests")
+    nose.main(config=nose.config.Config(verbosity=2))
