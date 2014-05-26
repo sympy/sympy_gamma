f158fd5526d0531e720eb130f056bbb6ccfa61d3
sahilshekhawat <sahilshekhawat01@gmail.com>
2014-05-26 20:28:14 +0530

    Added cards to be converted into json

--- app/notebook.py
+++ app/notebook.py
@@ -1,16 +1,12 @@
 from __future__ import absolute_import
 from django.http import HttpResponse
-from django.shortcuts import render
 from copy import copy
 from app.logic import SymPyGamma
+from app.views import eval_card
 import json
+import urllib2
 
-notebook_format = { "metadata": {"name": ""}, "nbformat": 3, "nbformat_minor": 0, "worksheets": [{ "cells": [],"metadata": {} }]}
-code_cell = {"cell_type": "code",  "input": [], "language": "python", "metadata": {}, "outputs": [{ "output_type": "stream","stream": "stdout", "text": []}], "prompt_number": 1 },
-markdown_cell = {"cell_type": "markdown","metadata": {},"source":[]},
-heading_cell = {"cell_type": "heading","level": 3,"metadata": {},"source": []},
-
-#<p id="heading">SymPy</p>   #styling for notebook to center it.
+#<p id="heading">SymPy</p>   #styling for notebook's headings.
 #<style>
 #    #heading {
 #        text-align:center;
@@ -19,83 +15,99 @@ heading_cell = {"cell_type": "heading","level": 3,"metadata": {},"source": []},
 #    }
 #</style>
 
+def result_json(request):
 
-def result_pass(request):
-    ''' This function parses and creates the result's json for nbviewer '''
+    ''' IPython notebook format generator. Parses the result set and
+        converts them into IPython notebook's format. '''
+
     notebook_format = { "metadata": {"name": ""}, "nbformat": 3, "nbformat_minor": 0, "worksheets": [{ "cells": [],"metadata": {} }]}
-    notebook = notebook_format
-    inp = request.GET.get('i')
+    code_cell = {"cell_type": "code",  "input": [], "language": "python", "metadata": {}, "outputs": [{ "output_type": "stream","stream": "stdout", "text": []}], "prompt_number": 1 },
+    markdown_cell = {"cell_type": "markdown","metadata": {},"source":[]},
+    heading_cell = {"cell_type": "heading","level": 3,"metadata": {},"source": []},
+
+    exp = request.GET.get('i')
+    exp = urllib2.unquote(exp)
     g = SymPyGamma()
-    result = g.eval(inp)
-    #----------------------------------------------------------
-    # 1)this is in no way completed. we need to implement plotting
-    # and cards. cards can be implemented by using exec
-    # statements like
-    # a = ''' import sympy
-    #         x = Symbol('x')
-    #         series(tan(x + 1), x, 0, 10)'''
-    # for plotting we can use existing support of matplotlib on
-    # Google App engine.
-    #-------------------------------------------------------------
-    for q in range(len(result)):
-        cell = result[q]
+    result = g.eval(exp)
+    notebook = copy(notebook_format)
+
+    for cell in result:
         if 'ambiguity' in cell.keys():
+
             ambiguity = copy(markdown_cell[0])
-            description = heading_cell[0]
             card_link = cell['ambiguity']
-            link = '<p>Did you mean: <a href="/input/?i=' + str(card_link) + '>"' + str(card_link) + '</a>?</p>'
-            ambiguity['source'] = link
-            description['source'] = cell['description']
+            link = '<p>Did you mean: <a href="http://sympygamma.com/input/?i=' + str(card_link) + '>"' + str(card_link) + '</a>?</p>'
+            ambiguity['source'] = [link]
             notebook['worksheets'][0]['cells'].append(ambiguity)
-            notebook['worksheets'][0]['cells'].append(description)
+
+            if cell['description'] != '':
+                description = heading_cell[0]
+                description['source'] = [cell['description']]
+                notebook['worksheets'][0]['cells'].append(description)
+
         else:
             title = copy(heading_cell[0])
-            title['level'] = 3
+            title['level'] = 5
             a = str(cell['title'])
-            title['source'] = [ a ]
+            title['source'] = [a]
             notebook['worksheets'][0]['cells'].append(title)
+
             if 'input' in cell.keys():
                 inputs = copy(heading_cell[0])
-                inputs['level'] = 3
-                inputs['source'] = [ str(cell['input']) ]
+                inputs['level'] = 5
+                inputs['source'] = [str(cell['input'])]
                 notebook['worksheets'][0]['cells'].append(inputs)
+
             if 'output' in cell.keys():
                 output = copy(markdown_cell[0])
-                if cell['output'] != "":
-                    strip = cell['output']
-                    a = strip.find('>')
-                    strip = strip[a+1:]
-                    b = strip.find('<')
-                    strip = strip[:b]
-                    output['source'] = '$$' + str(strip) + '$$'
-                    notebook['worksheets'][0]['cells'].append(output)
+
                 if 'pre_output' in cell.keys():
                     if cell['pre_output'] !="":
                         pre_output = copy(markdown_cell[0])
-                        pre_output['source'] = '$$'+str(cell['pre_output'])+'$$'
+                        pre_output['source'] = ['$$'+str(cell['pre_output'])+'$$']
                         notebook['worksheets'][0]['cells'].append(pre_output)
+
+                if cell['output'] != "" and 'script' in cell['output']:
+                    output['source'] = [cell['output']]
+                    notebook['worksheets'][0]['cells'].append(output)
+
             if 'card' in cell.keys():
+                card_name = cell['card']
+                variable = cell['var']
+
                 if 'pre_output' in cell.keys():
-                    if cell['pre_output'] != "":
+                    if cell['pre_output'] != '':
                         cell_pre_output = copy(markdown_cell[0])
-                        cell_pre_output['source'] = '$$'+str(cell['pre_output'])+'$$'
+                        cell_pre_output['source'] = ['$$'+str(cell['pre_output'])+'$$']
                         notebook['worksheets'][0]['cells'].append(cell_pre_output)
+                # using "format" instread of string catenation returns entire HTML source.
+                try:
+                    card_json = (urllib2.urlopen('http://sympygamma.com/card/'+ str(card_name)
+                                + '?variable=' + str(variable) + '&expression=' + str(exp))).read()
+                    card_json = json.loads(card_json)
+                    card_result = copy(markdown_cell[0])
+                    card_result['source'] = [card_json['output']]
+                    notebook['worksheets'][0]['cells'].append(card_result)
+                except:
+                    pass
+
             if 'cell_output' in cell.keys():
                 if cell['cell_output'] != "":
                     cell_output = copy(markdown_cell[0])
-                    cell_output['source'] = cell['cell_output']
+                    cell_output['source'] = [cell['cell_output']]
                     notebook['worksheets'][0]['cells'].append(cell_output)
+
             elif 'pre_output' in cell.keys():
                 if cell['pre_output'] != "":
                     pre_output = copy(markdown_cell[0])
-                    pre_output['source'] = '$$' + str(cell['pre_output']) + '$$'
+                    pre_output['source'] = ['$$' + str(cell['pre_output']) + '$$']
                     notebook['worksheets'][0]['cells'].append(pre_output)
+
             else:
                     pass
 
-    notebook = json.dumps(notebook)
-    return render(request, 'result_notebook.ipynb',
-                      {'result': result,
-                       'notebook': notebook,
-                       'result': result},
-                        content_type = "text/plain")
+    notebook_json = json.dumps(notebook)
+    response =  HttpResponse(notebook_json, content_type = 'application/json')
+    #Comment following line to stop the file from being downloaded.
+    response['Content-Disposition'] = 'attachment; filename=gamma.ipynb'
+    return response
