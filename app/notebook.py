e26cdb9e90c691181b5f90a430346c9aa6884359
sahilshekhawat <sahilshekhawat01@gmail.com>
2014-06-10 13:53:56 +0530

    Fixed issues related to formatting

--- app/notebook.py
+++ app/notebook.py
@@ -39,7 +39,7 @@ def result_json(request):
         converts them into IPython notebook's format. '''
 
     notebook_format = { "metadata": {"name": ""}, "nbformat": 3, "nbformat_minor": 0, "worksheets": [{ "cells": [],"metadata": {} }]}
-    code_cell = {"cell_type": "code", "collapsed": "false", "input": [], "language": "python", "metadata": {}, "outputs": [{ "output_type": "pyout", "text": [], "prompt_number": 1}], "prompt_number": 1 },
+    code_cell = {"cell_type": "code", "input": [], "language": "python", "metadata": {}, "outputs": [{ "output_type": "pyout", "text": [], "prompt_number": 1}], "prompt_number": 1 },
     markdown_cell = {"cell_type": "markdown","metadata": {},"source":[]},
     heading_cell = {"cell_type": "heading","level": 3,"metadata": {},"source": []},
 
@@ -58,7 +58,7 @@ def result_json(request):
         title['source'] = [a]
         notebook['worksheets'][0]['cells'].append(title)
 
-        if 'input' in cell.keys():
+        if 'input' in cell:
             if cell['input'] != None and cell['input'] != '':
                 inputs = copy(code_cell[0])
                 inputs['input'] = [ str(cell['input']) ]
@@ -66,10 +66,10 @@ def result_json(request):
                 prompt_number = prompt_number + 1
                 notebook['worksheets'][0]['cells'].append(inputs)
 
-        if 'output' in cell.keys():
+        if 'output' in cell:
             output = copy(markdown_cell[0])
 
-            if 'pre_output' in cell.keys():
+            if 'pre_output' in cell:
                 if cell['pre_output'] !="" and cell['pre_output'] != 'None':
                     pre_output = copy(markdown_cell[0])
                     pre_output['source'] = ['$$'+str(cell['pre_output'])+'=$$']
@@ -79,12 +79,12 @@ def result_json(request):
                 output['source'] = [cell['output']]
                 notebook['worksheets'][0]['cells'].append(output)
 
-        if 'card' in cell.keys():
+        if 'card' in cell:
             if cell['card'] != 'plot':
                 card_name = cell['card']
                 variable = cell['var']
                 parameters = {}
-                if 'pre_output' in cell.keys():
+                if 'pre_output' in cell:
                     if cell['pre_output'] != '' and cell['pre_output'] != 'None':
                         cell_pre_output = copy(markdown_cell[0])
                         cell_pre_output['source'] = ['$$'+str(cell['pre_output'])+ '=$$']
@@ -97,11 +97,28 @@ def result_json(request):
                     parser = Parser()
                     parser.feed(card_json_output)
                     parsed_cell_inputs = parser.cell_input #list of data values with <div class='cell_input'>
+                    card_json_output = [card_json_output]
 
                     for card_cell_input in parsed_cell_inputs:
-                        #output_test.append(card_cell_input)   #for testing
-
                         if card_cell_input != '\n' and card_cell_input != '</div>' and card_cell_input != '\\':
+
+                            card_json_output = card_json_output[0].split(card_cell_input)
+                            try:    #removing <ul>'s, <li>'s and <div>'s
+                                if card_json_output[0][:7] == '</div>\n':
+                                    card_json_output[0] = card_json_output[0][7:]
+                                if card_json_output[0][:7] == '\n</div>':
+                                    card_json_output[0] = card_json_output[0][7:]
+                                if card_json_output[0][:10] == '<ul>\n<li>\n':
+                                    card_json_output[0] = card_json_output[0][10:]
+                                if card_json_output[0][:9] == '<ul>\n<li>':
+                                    card_json_output[0] = card_json_output[0][19:]
+                                if card_json_output[0][-24:] == '<div class=\"cell_input\">':
+                                    card_json_output[0] = card_json_output[0][:-24]
+                            except:
+                                pass
+                            card_result = copy(markdown_cell[0])
+                            card_result['source'] = [card_json_output[0]]
+                            notebook['worksheets'][0]['cells'].append(card_result)  #storing output after input
 
                             if card_cell_input[:1] == '\n':
                                 card_cell_input = card_cell_input[1:]
@@ -114,25 +131,23 @@ def result_json(request):
                             prompt_number = prompt_number + 1
                             notebook['worksheets'][0]['cells'].append(card_heading)
 
-                            card_json_output = card_json_output.split(card_cell_input)
-                            try:
-                                if card_json_output[1][:7] == '\n</div>':
-                                    card_json_output[1] = card_json_output[1][7:]
-                            except:
-                                pass
-
-                            card_result = copy(markdown_cell[0])
-                            card_result['source'] = [card_json_output[1]]
-                            notebook['worksheets'][0]['cells'].append(card_result)  #storing output after input
-
                         if len(card_json_output) > 1 :
-                            card_json_output = card_json_output[1]
+                            card_json_output = [card_json_output[1]]
                         else:
-                            card_json_output = card_json_output[0]
+                            card_json_output = [card_json_output[0]]
 
-                    if card_json_output != '<':
+                    if card_json_output[0] != '<':
+                        try:
+                            if card_json_output[0][:7] == '</div>\n':
+                                card_json_output[0] = card_json_output[0][7:]
+                            if card_json_output[0][:10] == '<ul>\n<li>':
+                                card_json_output[0] = card_json_output[0][10:]
+                            if card_json_output[0][:9] == '<ul>\n<li>':
+                                card_json_output[0] = card_json_output[0][9:]
+                        except:
+                            pass
                         card_last_result = copy(markdown_cell[0])
-                        card_last_result['source'] = [card_json_output]
+                        card_last_result['source'] = [card_json_output[0]]
                         notebook['worksheets'][0]['cells'].append(card_last_result)
                 except:
                     card_error = copy(markdown_cell[0])
@@ -143,27 +158,27 @@ def result_json(request):
                 card_plot['source'] = ['Plotting is not yet implemented.']
                 notebook['worksheets'][0]['cells'].append(card_plot)
 
-        if 'cell_output' in cell.keys():
+        if 'cell_output' in cell:
             if cell['cell_output'] != "":
                 cell_output = copy(markdown_cell[0])
                 cell_output['source'] = [cell['cell_output']]
                 notebook['worksheets'][0]['cells'].append(cell_output)
 
-        if 'error' in cell.keys():
+        if 'error' in cell:
             cell_error = copy(markdown_cell[0])
             cell_error['source'] = [cell['error']]
             notebook['worksheets'][0]['cells'].append(cell_error)
 
         else:
-                pass
+            pass
 
-
+    #styling for the notebook (Css)
     notebook_styling = copy(markdown_cell[0])
     notebook_styling['source'] = [styling]
     notebook['worksheets'][0]['cells'].append(notebook_styling)
-
+    #Converting it into the json format.
     notebook_json = json.dumps(notebook)
     response =  HttpResponse(notebook_json, content_type = 'text/plain')
+    #uncomment the following line to display json rather than downloading it.
     response['Content-Disposition'] = 'attachment; filename=gamma.ipynb'
     return response
-
