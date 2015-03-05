fd70958b3015c095d3fa9d733d6682ce8ecdc931
David Li <li.davidm96@gmail.com>
2015-03-04 22:31:26 -0500

    Clean up URL patterns

--- urls.py
+++ urls.py
@@ -1,4 +1,4 @@
-from django.conf.urls.defaults import *
+from django.conf.urls import patterns, url
 
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
@@ -9,29 +9,20 @@ p = os.path.join(os.path.dirname(__file__), 'media/')
 
 urlpatterns = patterns(
     '',
-    # Example:
-    # (r'^notebook/', include('notebook.foo.urls')),
-    (r'^$', 'app.views.index'),
 
-    (r'^input/', 'app.views.input'),
-    (r'^about/$', 'app.views.about'),
-    (r'^random', 'app.views.random_example'),
+    url(r'^$', 'app.views.index'),
 
-    (r'user/remove/(?P<qid>.*)$', 'app.views.remove_query'),
+    url(r'^input/', 'app.views.input'),
+    url(r'^about/$', 'app.views.about'),
+    url(r'^random', 'app.views.random_example'),
 
-    (r'card/(?P<card_name>\w*)$', 'app.views.eval_card'),
+    url(r'user/remove/(?P<qid>.*)$', 'app.views.remove_query'),
 
-    (r'card_info/(?P<card_name>\w*)$', 'app.views.get_card_info'),
+    url(r'card/(?P<card_name>\w*)$', 'app.views.eval_card'),
 
-    (r'card_full/(?P<card_name>\w*)$', 'app.views.get_card_full')
+    url(r'card_info/(?P<card_name>\w*)$', 'app.views.get_card_info'),
 
-
-    # Uncomment the admin/doc line below and add 'django.contrib.admindocs'
-    # to INSTALLED_APPS to enable admin documentation:
-    # (r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
-    # Uncomment the next line to enable the admin:
-    # (r'^admin/(.*)', admin.site.root),
+    url(r'card_full/(?P<card_name>\w*)$', 'app.views.get_card_full')
 )
 
 handler404 = 'app.views.view_404'
