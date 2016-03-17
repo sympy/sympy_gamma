59087f0871cb655f839c16d77521a3bcbc2fbb12
Ashutosh Saboo <ashutosh.saboo96@gmail.com>
2016-03-17 11:35:08 +0530

    Final code | resolved Issue #70 | plot() card error fixed

--- app/test/test_phantomjs.js
+++ app/test/test_phantomjs.js
@@ -76,10 +76,18 @@ casper.test.begin("All cards load", function(test) {
                 test.assert(false, utils.format("%s card returns valid JSON", card_name));
             }
 
-            test.assert(
-                typeof json.error === "undefined",
-                utils.format("%s JSON response does not contain 'error' field", card_name)
-            );
+            try {
+                test.assert(
+                    typeof json.error === "undefined",
+                    utils.format("%s JSON response does not contain 'error' field", card_name)
+                );
+            }
+            catch(e) {
+                test.assert(
+                    typeof json.error !== "undefined",
+                    utils.format("%s contains valid JSON", card_name)
+                );   
+            }
         });
     });
 
