5ce03d060d5c53422543b0e698e947b2881def2e
Amit Kumar <dtu.amit@gmail.com>
2020-04-22 00:44:10 +0100

    Add more logging in update pr status script

--- bin/update_status_on_pr.py
+++ bin/update_status_on_pr.py
@@ -28,7 +28,9 @@ def update_pr_status_with_deployment(branch_name, commit_sha):
         'Content-Type': 'application/x-www-form-urlencoded'
     }
 
-    response = requests.request("POST", GITHUB_API_UPDATE_STATUS_URL + commit_sha, headers=headers, data=payload)
+    update_status_url = GITHUB_API_UPDATE_STATUS_URL + commit_sha
+    print "Update status URL: %s" % update_status_url
+    response = requests.request("POST", update_status_url, headers=headers, data=payload)
     print "Response: %s" % response.json()
 
 
@@ -38,6 +40,7 @@ def main():
         raise ValueError('This script run only on travis!')
     branch_name = os.environ.get('TRAVIS_BRANCH')
     commit_sha = get_branch_commit_sha(branch_name)
+    print "Branch name: %s Commit SHA: %s" % branch_name, commit_sha
     update_pr_status_with_deployment(branch_name, commit_sha)
 
 
